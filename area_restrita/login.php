c4988d85e82308f5b3ee2f6937b87d05b2735d3c
Neto (Geraldo Cezário Neto) <geraldocezario@gmail.com>
2025-05-27 16:07:41 -0300

    Atualização

---
+++ area_restrita/login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Área Restrita - Login</title>
+
+    <link rel="stylesheet" href="../tools/fontawesome/css/all.css" />
+    <link rel="stylesheet" href="css/login.css" />
+</head>
+<body>
+    <div id="interface">
+        <form action="" method="POST">
+            <span class="login-user"><i class="fas fa-user"></i></span>
+
+            <input type="email" name="txt_email" id="txt_email" class="" placeholder="Digite seu e-mail" required />
+            <input type="password" name="txt_pw" id="txt_pw" class="" placeholder="Digite sua senha" required />
+
+            <input type="submit" name="bt-submit" id="bt-submit" value="Entrar" />
+        </form>
+    </div>
+</body>
+</html>
--- index.php
+++ index.php
@@ -5,7 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Portifólio</title>
 
+    <link rel="stylesheet" href="tools/fontawesome/css/all.css" />
     <link rel="stylesheet" href="css/estilos.css" />
+
+    <script src="tools/jquery/jquery.js"></script>
+
+    <script>
+        
+    </script>
 
 </head>
 <body>
@@ -17,37 +24,237 @@
         <nav class="menu">
 
             <div class="logo">
-                <img src="imagens/logo.png" alt="LOGO" width="150" />
+                <img src="imagens/logo.png" alt="LOGO" />
             </div>
 
             <ul>
-                <li>Home</li>
-                <li>Serviços</li>
-                <li>Produtos</li>
-                <li>Portifólio</li>
-                <li>Sobre</li>
-                <li>Contato</li>
+                <li><a href="#"><i class="fas fa-home"></i>Home</a></li>
+                <li><a href="#servicos"><i class="fas fa-tasks"></i>Serviços</a></li>
+                <li><a href="#"><i class="fas fa-photo-video"></i>Portifólio</a></li>
+                <li><a href="#"><i class="fas fa-user-friends"></i>Sobre</a></li>
+                <li><a href="#"><i class="fas fa-comments"></i>Contato</a></li>
+                <li><a href="area_restrita/"><i class="fas fa-unlock-alt"></i>Restrito</a></li>
             </ul>
 
         </nav>
+        
+        <div class="titles"> <!-- ========================= -->
+            <div class="container-titles">
+                <h1 class="titulo">Entregando Serviços de Qualidade</h1>
+                <h1 class="subtitulo">Soluções em TI</h1>
 
-        <div class="titles">
+                <p class="paragrafo">Lorem ipsum dolor sit amet consectetur adipisicing elit. Id vitae rerum, necessitatibus quod quaerat mollitia.</p>
+                
+            </div>
 
-            <h1 class="titulo">Entregando serviços de qualidade</h1>
-            <h1 class="subtitulo">Soluções em TI</h1>
-
-            <p class="paragrafo">Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam quibusdam ab in! A, nam voluptas!</p>
-
-        </div>
-
-        <div class="socialIcons">
-            <a href="#">Twitter</a>
-            <a href="#">Facebook</a>
-            <a href="#">LinkedIn</a>
-            <a href="#">Instagram</a>
-        </div>
-
+            <div class="socialIcons">
+                <a href="#" title="Visite nosso Twitter"><i class="fab fa-twitter"></i></a>
+                <a href="#" title="Visite nosso Facebook"><i class="fab fa-facebook"></i></a>
+                <a href="#" title="Visite nosso LinkedIn"><i class="fab fa-linkedin"></i></a>
+                <a href="#" title="Visite nosso Instagram"><i class="fab fa-instagram"></i></a>
+            </div>
+        </div> <!-- ========================== -->
+        
     </header>
+
+    <!-- ========================================================= -->
+
+    <main id="corpo">
+
+        <section id="servicos" class="servicos">
+
+            <article class="servico">
+                <img src="imagens/service-design.svg" alt="Design">
+                <h3 class="title-servico">Desenvolvimento WEB</h3>
+                <p class="text-servico">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis omnis, voluptas error impedit consectetur vitae.</p>
+            </article>
+            <article class="servico">
+                <img src="imagens/service-hosting.svg" alt="Hosting">
+                <h3 class="title-hosting">WEB Hosting</h3>
+                <p class="text-servico">Possimus vel temporibus deleniti repudiandae in similique unde necessitatibus odit corporis nihil soluta, ad repellendus?</p>
+            </article>
+            <article class="servico">
+                <img src="imagens/service-social.svg" alt="Social Media">
+                <h3 class="title-servico">Mídias Sociais</h3>
+                <p class="text-servico">Earum mollitia a, quaerat magni dignissimos, sint velit libero minus magnam saepe perferendis aspernatur? Libero!</p>
+            </article>
+            <article class="servico">
+                <img src="imagens/service-seo.svg" alt="SEO">
+                <h3 class="title-servico">Aplicações SEO</h3>
+                <p class="text-servico">Maiores aliquid sequi expedita. Rerum itaque nihil maxime at explicabo similique recusandae saepe magnam a!</p>
+            </article>
+            <article class="servico">
+                <img src="imagens/service-cloud.svg" alt="Cloud">
+                <h3 class="title-servico">Computação em Nuvem</h3>
+                <p class="text-servico">Provident velit fuga consequuntur quae, iste accusamus repellendus, officiis quibusdam, odio doloribus voluptates iusto perspiciatis.</p>
+            </article>
+            <article class="servico">
+                <img src="imagens/service-secure.svg" alt="Security">
+                <h3 class="title-servico">Segurança de Dados</h3>
+                <p class="text-servico">Expedita illo fugit error in quidem qui, architecto eligendi quae, perspiciatis modi non ab aliquid.</p>
+            </article>
+
+        </section>
+
+        <section class="portifolio">
+
+            <h3 class="title-portifolio">Nosso Portifólio</h3>
+            <p class="sub-title">Lorem ipsum dolor sit amet.</p>
+
+            <div class="container-imagens">
+                <a href="#" class="img" style="background-image: url('imagens/product-1.jpg');">
+                    
+                </a>
+                <a href="#" class="img" style="background-image: url('imagens/product-2.jpg');">
+                    
+                </a>
+                <a href="#" class="img" style="background-image: url('imagens/product-3.jpg');">
+                    
+                </a>
+                <a href="#" class="img" style="background-image: url('imagens/product-4.jpg');">
+                    
+                </a>
+                <a href="#" class="img" style="background-image: url('imagens/product-5.jpg');">
+
+                </a>
+                <a href="#" class="img" style="background-image: url('imagens/product-6.jpg');">
+                    
+                </a>
+            </div>
+
+        </section>
+
+        <section class="banner">
+
+            <hgroup class="container-texto">
+
+                <h4 class="title-banner">Saiba mais sobre nossos Projetos</h4>
+
+                <p class="txt-banner">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat similique, placeat maxime odio voluptas ducimus pariatur libero accusamus, ad aperiam quis et repellendus cupiditate nostrum.</p>
+
+                <a href="#" class="btn-banner">Saiba mais</a>
+
+            </hgroup>
+
+        </section>
+
+        <section class="sobre">
+
+
+
+        </section>
+
+        <section class="contato">
+
+            <h3 class="title-contato">Fale conosco</h3>
+            <p class="sub-title">Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+
+            <div class="container-contato">
+
+                <aside class="dados-contato">
+                    <h4 class="title-aside">Endereço</h4>
+                    <p class="txt-aside">Rua das Margaridas, S/Nº<br />Centro - Manhuaçu/MG</p>
+
+                    <h4 class="title-aside">Telefone</h4>
+                    <p class="txt-aside">33 3331-3333</p>
+                    <p class="txt-aside">33 9 8400-9999</p>
+
+                    <h4 class="title-aside">E-Mail</h4>
+                    <p class="txt-aside">contato@meusite.com.br</p>
+                    <p class="txt-aside">faleconosco@meusite.com.br</p>
+                </aside>
+
+                <form action="" method="POST">
+
+                    <div class="line-form">
+                        <input type="text" name="nome" id="nome" placeholder="Nome*" required />
+                        <input type="email" name="email" id="email" placeholder="E-Mail*" required />
+                    </div>
+
+                    <div class="line-form">
+                        <input type="text" name="assunto" id="assunto" placeholder="Assunto" />
+                    </div>
+
+                    <div class="line-form">
+                        <textarea name="mensagem" id="mensagem" rows="7" placeholder="Escreva Sua Mensagem*" required></textarea>
+                    </div>
+
+                    <button type="submit">Enviar mensagem</button>
+
+                </form>
+
+            </div>
+
+        </section>
+
+    </main>
+
+    <!-- ========================================================= -->
+
+    <footer id="rodape">
+
+        <div class="sombreamento">
+
+            <div class="container-rodape">
+
+                <section class="item-rodape sobre">
+
+                    <img src="imagens/logo.png" alt="Logo" />
+
+                    <p class="txt-sobre">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci reiciendis itaque nihil magni excepturi, assumenda in dolore iure necessitatibus deserunt.</p>
+
+                    <h4 class="title-sobre">Siga-nos</h4>
+
+                    <div class="social-icons">
+                        <a href="#" title="Visite nosso Twitter"><i class="fab fa-twitter"></i></a>
+                        <a href="#" title="Visite nosso Facebook"><i class="fab fa-facebook"></i></a>
+                        <a href="#" title="Visite nosso LinkedIn"><i class="fab fa-linkedin"></i></a>
+                        <a href="#" title="Visite nosso Instagram"><i class="fab fa-instagram"></i></a>
+                    </div>
+
+                </section>
+
+                <section class="item-rodape servicos">
+
+                    <h4 class="title-servicos">Serviços</h4>
+
+                    <ul>
+                        <li><a href="#">Desenvolvimento WEB</a></li>
+                        <li><a href="#">WEB Hosting</a></li>
+                        <li><a href="#">Mídias Sociais</a></li>
+                        <li><a href="#">Aplicações SEO</a></li>
+                        <li><a href="#">Computação em Nuvem</a></li>
+                        <li><a href="#">Segurança de Dados</a></li>
+                    </ul>
+
+                </section>
+
+                <section class="item-rodape contato">
+
+                    <h4 class="title-contato">Contatos</h4>
+
+                    <div class="container-contato">
+                        <p class="txt-contatos">Rua das Margaridas, S/Nº</p>
+                        <p class="txt-contatos">33 9 8400-9999</p>
+                        <p class="txt-contatos">contato@meusite.com.br</p>
+                    </div>
+
+                    <h4 class="title-contato">Newsletter</h4>
+                    <form action="" method="GET">
+                        <input type="email" name="news-email" id="news-email" placeholder="E-Mail" />
+
+                        <button type="submit"><i class="fab fa-telegram"></i></button>
+                    </form>
+
+                </section>
+
+            </div>
+
+            <p class="txt-creditos">Geraldo Cezário Neto &copy; 2025</p>
+
+        </div>
+
+    </footer>
 
 </div>
     
